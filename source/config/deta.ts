d1af7d1380e3e4c95b6afe51964402075766cc12
mythcsj <mythcsj@163.com>
2023-08-25 11:37:26 +0800

    [fix] modify code

--- source/config/deta.ts
+++ source/config/deta.ts
@@ -34,22 +34,39 @@ export class EnvMap {
 }
 
 export async function getEnv(key: string) {
-    const deta = Deta();
-    const db = deta.Base('data');
+    const db = await getDataBase('data');
     const res = await db.get(key);
     let value = '';
     if (!res) {
         value = await getRemoteEnv(key);
-        if (value) {
-            await db.put({ value }, key);
-        }
+        if (value) await db.put({ value }, key);
+    } else {
+        value = res['value'].toString();
     }
     return value.toString();
 }
 
+async function getDataBase(baseName: string) {
+    const deta = Deta();
+    const db = deta.Base(baseName);
+    const { count } = await db.fetch();
+    if (count == 0) {
+        const putOne = await db.put({ value: 'value' }, 'key');
+        if (putOne) await db.delete('key');
+    }
+    return db;
+}
+
 async function getRemoteEnv(constKey: string) {
-    const { data } = await axios(
-        `${AUTH_CENTER_API}/api/env?constKey=${constKey}`
+    const db = await getDataBase('config');
+    const { status, data } = await axios(
+        `${AUTH_CENTER_API}/api/env?constKey=${constKey}`,
+        {
+            headers: {
+                'auth-token': (await db.get('AUTH-TOKEN'))['value'].toString()
+            }
+        }
     );
+    if (status != 200) return '';
     return data['data'];
 }
